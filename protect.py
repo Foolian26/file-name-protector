53fa4d81fc39347f05ad87527d28dcda5c5c90d5
Foolian <94489538+Foolian26@users.noreply.github.com>
2022-09-10 11:30:00 +0200

    Update protect.py

--- protect.py
+++ protect.py
@@ -4,14 +4,14 @@ import asyncio
 name = os.path.basename(__file__)
 
 class Foolian:
-    def Foolian_exit(self):
+    def exit(self):
         os._exit(0)
 
     async def init(self):
         if name == 'protect.py':
             pass
         else:
-            self.Foolian_exit()
+            self.exit()
 asyncio.run(Foolian().init())
 #put your code here
 print("Hello World")
